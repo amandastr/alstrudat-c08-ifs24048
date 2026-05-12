7f4324c7eec303d2a86a682314ea844baf2a6757
amandastr <amandashintasitorus@gmail.com>
2026-05-12 14:33:40 +0700

    init

--- README.md
+++ README.md
@@ -61,3 +61,5 @@ Catatan STATS:
 
 ## Run
 `mvn exec:java`
+
+
--- src/main/java/del/alstrudat/App.java
+++ src/main/java/del/alstrudat/App.java
@@ -1,10 +1,6 @@
 package del.alstrudat;
 
 import java.util.Scanner;
-\
-
-
-
 
 public class App {
     public static void main(String[] args) {

22e1a8546e635602939bd34c068cecb899aa45c9
amandastr <amandashintasitorus@gmail.com>
2026-05-12 14:59:18 +0700

    try to solvee!

--- src/main/java/del/alstrudat/Program.java
+++ src/main/java/del/alstrudat/Program.java
@@ -37,76 +37,26 @@ public class Program {
     private double totalPrioritas = 0;
 
     public void enqueueIGD(String nama, int prioritas) {
-        counter++;
-        antrianIGD.add(new Pasien(nama, prioritas, counter, "IGD"));
+        // TODO: implement
     }
 
     public void enqueueReg(String nama, int prioritas) {
-        counter++;
-        antrianReg.add(new Pasien(nama, prioritas, counter, "REG"));
+        // TODO: implement
     }
 
     public void serve() {
-        if (antrianIGD.isEmpty() && antrianReg.isEmpty()) {
-            System.out.println("Semua antrian kosong");
-            return;
-        }
-
-        Pasien p;
-        if (!antrianIGD.isEmpty()) {
-            p = antrianIGD.poll();
-            igdDilayani++;
-            System.out.println("[IGD] Dilayani: " + p.nama + " (Prioritas: " + p.prioritas + ")");
-        } else {
-            p = antrianReg.poll();
-            regDilayani++;
-            System.out.println("[REG] Dilayani: " + p.nama + " (Prioritas: " + p.prioritas + ")");
-        }
-
-        totalDilayani++;
-        totalPrioritas += p.prioritas;
-        riwayat.push(p);
+        // TODO: implement
     }
 
     public void undo() {
-        if (riwayat.isEmpty()) {
-            System.out.println("Tidak ada riwayat");
-            return;
-        }
-
-        Pasien p = riwayat.pop();
-        if (p.asalAntrian.equals("IGD")) {
-            antrianIGD.add(p);
-            System.out.println("UNDO: " + p.nama + " dikembalikan ke antrian IGD");
-        } else {
-            antrianReg.add(p);
-            System.out.println("UNDO: " + p.nama + " dikembalikan ke antrian Reguler");
-        }
+        // TODO: implement
     }
 
     public void peek() {
-        if (antrianIGD.isEmpty() && antrianReg.isEmpty()) {
-            System.out.println("Semua antrian kosong");
-            return;
-        }
-
-        if (!antrianIGD.isEmpty()) {
-            Pasien p = antrianIGD.peek();
-            System.out.println("Berikutnya [IGD]: " + p.nama + " (Prioritas: " + p.prioritas + ")");
-        } else {
-            Pasien p = antrianReg.peek();
-            System.out.println("Berikutnya [REG]: " + p.nama + " (Prioritas: " + p.prioritas + ")");
-        }
+        // TODO: implement
     }
 
     public void stats() {
-        System.out.println("Total dilayani: " + totalDilayani);
-        if (totalDilayani == 0) {
-            System.out.println("Rata-rata prioritas: 0.00");
-        } else {
-            System.out.printf("Rata-rata prioritas: %.2f%n", totalPrioritas / totalDilayani);
-        }
-        System.out.println("Pasien IGD dilayani: " + igdDilayani);
-        System.out.println("Pasien Reguler dilayani: " + regDilayani);
+        // TODO: implement
     }
 }
